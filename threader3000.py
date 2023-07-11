4c7aef436fb9a24fa875b6c6dae2231e05b2ffdc
Robert Wu <rocklee.au@foxmail.com>
2023-07-11 17:25:19 +1000

    Allow user to tweaking the default timeout for socket; Allow using environment variables from bash for $TARGET_IP; Add confirmation before commencing the scanning; Add a progress indicator for better overview.

--- threader3000.py
+++ threader3000.py
@@ -21,35 +21,63 @@ from datetime import datetime
 
 # Main Function
 def main():
-    socket.setdefaulttimeout(0.30)
     print_lock = threading.Lock()
     discovered_ports = []
 
-# Welcome Banner
+   # Welcome Banner
     print("-" * 60)
     print("        Threader 3000 - Multi-threaded Port Scanner          ")
-    print("                       Version 1.0.7                    ")
-    print("                   A project by The Mayor               ")
+    print("                       Version 1.1.0                    ")
     print("-" * 60)
     time.sleep(1)
-    target = input("Enter your target IP address or URL here: ")
+
+   # Default time out
+    default_timeout = input("Set default timeout (press Enter for 0.3): ")
+    if default_timeout:
+        try:
+            default_timeout = float(default_timeout)
+        except ValueError:
+            print("Invalid timeout value. Defaulting to 0.3 seconds.")
+            default_timeout = 0.3
+    else:
+        default_timeout = 0.3
+        
+    socket.setdefaulttimeout(default_timeout) 
+   
+   # IP
+    target = os.getenv("TARGET_IP", "")
+    if not target:
+      target = input("Enter your target IP address or URL here: ")
     error = ("Invalid Input")
     try:
         t_ip = socket.gethostbyname(target)
     except (UnboundLocalError, socket.gaierror):
         print("\n[-]Invalid format. Please use a correct IP or web address[-]\n")
         sys.exit()
-    #Banner
+
+    #Summary
     print("-" * 60)
-    print("Scanning target "+ t_ip)
+    print("Settings Summary:")
+    print("-" * 60)
+    print(f"Target: {target}")
+    print(f"Default Timeout: {socket.getdefaulttimeout()} seconds")
+    print(f"Address Family: {socket.AF_INET}, Socket Type: {socket.SOCK_STREAM}")
+    print("-" * 60)
+    confirm = input("Start scanning with these settings? (y/n): ")
+    if confirm.lower() != "y":
+        main()
     print("Time started: "+ str(datetime.now()))
     print("-" * 60)
+
+
     t1 = datetime.now()
 
     def portscan(port):
+       with print_lock:
+         progress = (1 - (q.unfinished_tasks / 65535)) * 100
+         print(f"\{progress:.1f}% scanned", end='\r', flush=True)
 
        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       
        try:
           portx = s.connect((t_ip, port))
           with print_lock:
